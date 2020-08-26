c1a8fa50eb95c759c8694e1af16c521898c1eced
chris-cyr <chris.dana.cyr@gmail.com>
2020-08-25 23:01:06 -0500

    Day 2 of Java Curriculum

--- IdeaProjects/javaPractice/src/HelloWorld.java
+++ IdeaProjects/javaPractice/src/HelloWorld.java
@@ -2,6 +2,9 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.print("Hello");
-        System.out.print(", World!");
+        System.out.print(", World! \n");
+
+        int myFavoriteNumber = 17;
+        System.out.println(myFavoriteNumber);
     }
 }
