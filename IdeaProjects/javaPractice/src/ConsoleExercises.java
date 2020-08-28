586622cccb0e623ed7167c100c2a6fe86daa6621
chris-cyr <chris.dana.cyr@gmail.com>
2020-08-27 23:35:07 -0500

    Lesson 3 of Java

---
+++ IdeaProjects/javaPractice/src/ConsoleExercises.java
@@ -0,0 +1,11 @@
+public class ConsoleExercises {
+
+    public static void main(String[] args) {
+
+        double pi = 3.14159;
+
+
+        System.out.format("The value of pi is %. 2f ", pi);
+
+    }
+}
--- IdeaProjects/javaPractice/src/HelloWorld.java
+++ IdeaProjects/javaPractice/src/HelloWorld.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class HelloWorld {
 
     public static void main(String[] args) {
@@ -41,8 +43,19 @@ public class HelloWorld {
         //if you increment outside the bounds of a data type's allowed bounds, it rolls over to the negative and continues counting.
         System.out.println(potato);
 
+        String name = "Chris";
+        System.out.printf("Hey there, %s. Nice to see you. \n", name);
+
+        String greeting = "Howdy";
+        System.out.printf("%s, %s. \n", greeting, name);
 
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter something: ");
+        String userInput = scanner.nextLine();
+
+        System.out.println("You entered: -----> \"" + userInput + "\" <---");
 
 
     }
